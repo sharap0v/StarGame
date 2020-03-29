5bdd7f447afcb3207d1b017578721b1ffa037418
Sergey Zelentsov <_sharapov_@mail.ru>
2020-03-29 23:11:57 +0300

    logo move

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -9,19 +9,24 @@ import ru.geekbrains.base.BaseScreen;
 import ru.geekbrains.exception.GameException;
 import ru.geekbrains.math.Rect;
 import ru.geekbrains.sprites.Background;
+import ru.geekbrains.sprites.Logo;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture bg;
+    private Texture lg;
     private Background background;
+    private Logo logo;
     private Vector2 pos;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("textures/bg.png");
+        lg = new Texture("badlogic.jpg");
         try {
             background = new Background(bg);
+            logo = new Logo(lg);
         } catch (GameException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
@@ -39,21 +44,25 @@ public class MenuScreen extends BaseScreen {
     public void dispose() {
         batch.dispose();
         bg.dispose();
+        lg.dispose();
         super.dispose();
     }
 
     @Override
     public void resize(Rect worldBounds) {
-        background.resize(worldBounds);
+      background.resize(worldBounds);
+        logo.resize(worldBounds);
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
         pos.set(touch);
+        logo.touchDown(touch,pointer,button);
         return false;
     }
 
     private void update(float delta) {
+        logo.update(delta);
     }
 
     private void draw() {
@@ -61,6 +70,8 @@ public class MenuScreen extends BaseScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         background.draw(batch);
+        logo.draw(batch);
+        //batch.draw(lg,pos.x,pos.y);
         batch.end();
     }
 
