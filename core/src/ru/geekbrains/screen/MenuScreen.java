e308c6284515338f72b13eaf58b6aba8b52023f9
Sergey Zelentsov <_sharapov_@mail.ru>
2020-04-02 09:59:13 +0300

    classwork4

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -1,37 +1,45 @@
 package ru.geekbrains.screen;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
 import ru.geekbrains.exception.GameException;
 import ru.geekbrains.math.Rect;
 import ru.geekbrains.sprites.Background;
-import ru.geekbrains.sprites.Logo;
+import ru.geekbrains.sprites.ButtonExit;
+import ru.geekbrains.sprites.ButtonPlay;
+import ru.geekbrains.sprites.Star;
 
 public class MenuScreen extends BaseScreen {
 
+    private static final int STAR_COUNT = 256;
+
+    private final Game game;
+
     private Texture bg;
-    private Texture lg;
     private Background background;
-    private Logo logo;
-    private Vector2 pos;
+
+    private TextureAtlas atlas;
+
+    private Star[] stars;
+    private ButtonExit buttonExit;
+    private ButtonPlay buttonPlay;
+
+    public MenuScreen(Game game) {
+        this.game = game;
+    }
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("textures/bg.png");
-        lg = new Texture("badlogic.jpg");
-        try {
-            background = new Background(bg);
-            logo = new Logo(lg);
-        } catch (GameException e) {
-            e.printStackTrace();
-            throw new RuntimeException(e);
-        }
-        pos = new Vector2();
+        atlas = new TextureAtlas(Gdx.files.internal("textures/menuAtlas.tpack"));
+        initSprites();
     }
 
     @Override
@@ -44,25 +52,52 @@ public class MenuScreen extends BaseScreen {
     public void dispose() {
         batch.dispose();
         bg.dispose();
-        lg.dispose();
+        atlas.dispose();
         super.dispose();
     }
 
     @Override
     public void resize(Rect worldBounds) {
-      background.resize(worldBounds);
-        logo.resize(worldBounds);
+        background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
+        buttonExit.resize(worldBounds);
+        buttonPlay.resize(worldBounds);
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        pos.set(touch);
-        logo.touchDown(touch,pointer,button);
+        buttonExit.touchDown(touch, pointer, button);
+        buttonPlay.touchDown(touch, pointer, button);
         return false;
     }
 
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer, int button) {
+        buttonExit.touchUp(touch, pointer, button);
+        buttonPlay.touchUp(touch, pointer, button);
+        return false;
+    }
+
+    private void initSprites() {
+        try {
+            background = new Background(bg);
+            stars = new Star[STAR_COUNT];
+            for (int i = 0; i < STAR_COUNT; i++) {
+                stars[i] =  new Star(atlas);
+            }
+            buttonExit = new ButtonExit(atlas);
+            buttonPlay = new ButtonPlay(atlas, game);
+        } catch (GameException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private void update(float delta) {
-        logo.update(delta);
+        for (Star star : stars) {
+            star.update(delta);
+        }
     }
 
     private void draw() {
@@ -70,8 +105,11 @@ public class MenuScreen extends BaseScreen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         background.draw(batch);
-        logo.draw(batch);
-        //batch.draw(lg,pos.x,pos.y);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
+        buttonExit.draw(batch);
+        buttonPlay.draw(batch);
         batch.end();
     }
 
